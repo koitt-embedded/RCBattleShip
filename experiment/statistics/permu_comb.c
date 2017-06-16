debe1c0c4dcb7359f1acaa91b06859dcf4c6738c
silenc3502 <gcccompi3r@gmail.com>
2017-06-16 14:20:52 +0900

    complete permutation

---
+++ experiment/statistics/permu_comb.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+void size_check(void)
+{
+	printf("sizeof(int) = %lu\n", sizeof(int));
+	printf("sizeof(long) = %lu\n", sizeof(long));
+	printf("sizeof(long int) = %lu\n", sizeof(long int));
+	printf("sizeof(long double) = %lu\n", sizeof(long double));
+}
+
+long int factorial(int n)
+{
+	long int tmp = 1;
+
+	while(n)
+		tmp *= n--;
+
+	return tmp;
+}
+
+long int permutation(int n, int r)
+{
+	long int numerator = factorial(n);
+	long int denominator = factorial(n - r);
+	printf("numerator = %ld\n", numerator);
+	printf("denominator = %ld\n", denominator);
+
+	return numerator / denominator;
+}
+
+long int opt_permutation(int n, int r)
+{
+	int i;
+	int start = n;
+	int end = n - r;
+	int tmp = 1;
+
+	for(i = start; i > end; i--)
+		tmp *= i;
+
+	return tmp;
+}
+
+int main(void)
+{
+	long int res;
+
+	size_check();
+	res = permutation(20, 3);
+	printf("res = %ld\n", res);
+
+	res = opt_permutation(20, 3);
+	printf("res = %ld\n", res);
+
+	return 0;
+}
