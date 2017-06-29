731a96c9fa998b83e95d7f95dac2350a6ffe8fce
yunreka <yunreka@gmail.com>
2017-06-29 18:38:49 +0900

    opencv code source driving up

--- experiment/opencv/Gaussian_otsu_test.cpp
+++ experiment/opencv/Gaussian_otsu_test.cpp
@@ -13,13 +13,19 @@ void changeQuantisationGrey(Mat &image, int num_bits);
 
 void onmouse(int event, int x, int y, int flags, void* param)
 {
-//좌클릭할때 그위치에서 픽셀값과 위치를 출력
-        if(event == CV_EVENT_LBUTTONDOWN)
+//좌클릭할 때 (CV_EVENT_LBUTTONDOWN 부분) , 그 위치에서 픽셀값과 위치를 출력
+        if(event == CV_EVENT_LBUTTONDOWN) 
         {
                 Mat *img = (Mat*)(param);
                 cout <<"Mouse clicked" << (int)(*img).at<uchar>(y,x) << endl;
                 cout <<"Mouse clicked" << x << "," << y << endl;
         }
+//점찍으려면..
+	if(event == cv_EVENT_LBUTTONDOWN){
+		//원그리기 : 파라미터 ,  좌표  , 반지름,    빨간점   , 선의굵기
+		//           파라미터 는 Mat*으로 강제캐스팅 한 
+		circle(*(Mat*)param , point(x,y) , 2 , cv_RGB(255,0,0) , 3 );
+	}
 }
 //256단계의 gray 이미지를 2^num_bits 단계로 바꾸는 함수
 void changeQuantisationGrey(Mat &image, int num_bits)
@@ -42,14 +48,14 @@ int main(void)
 {
 	int CAM_ID = 0;
 	Mat frame;
-	Mat gray_image; //그레이체널 Mat 생성
+	Mat gray_image; //그레이체널 Mat 생성을 위한 변수선언
 	Mat bitable;
-	VideoCapture cap(CAM_ID); //카메라 생성
-	if(!cap.isOpened()){ //카메라 열렸는지 확인
+	VideoCapture cap(CAM_ID); //카메라 열기
+	if(!cap.isOpened()){ //카메라 열리지 않았다면, 아래 프린트문 실행하고 종료
 		printf("Can't open the CAM(%d)\n",CAM_ID);
 		return -1;
 	}
-	//이미지 해상도 얻기
+	//이미지 해상도 얻기 <- cap.size(width,height); 로 못받아오낭..? 찾아봐야함
 	double width = cap.get(CV_CAP_PROP_FRAME_WIDTH);
 	double height = cap.get(CV_CAP_PROP_FRAME_HEIGHT);
 	printf("size = [%f, %f]\n", width, height);
@@ -64,6 +70,9 @@ int main(void)
 	
 //	resizeWindow("Second_CAM" , 1080, 720);
 
+//	마우스 이벤트 수행을 위한 콜백함수 사용 부분 : 시스템이 이 함수를 호출해주고(ioctl일까..?)
+//							        -> 값을 넣어준다
+//	      마우스이벤트받을창이름,부를함수명,onmouse함수의 파라미터중-void* param 으로 넘겨줄값
 //	setMouseCallback("CAM_Window",onmouse, &gray_image);
 
 
@@ -74,13 +83,15 @@ int main(void)
 	
 
 	
-	cvtColor(frame, gray_image, CV_BGR2GRAY);  //컬러화면을 흑백으로 바꿔줌
+	cvtColor(frame, gray_image, CV_BGR2GRAY);  //컬러화면을 흑백(BGR순)으로 바꿔줌
 //	changeQuantisationGrey(gray_image, 3);
 
+//	가우시안블러.. 선명한 화질을 뿌옇게 보이게 흐리는 작업
 	GaussianBlur(gray_image , bitable ,Size(7,7),0,0);
+//	이진화	   입력영상,출력영상,thresh,max_value, 적용할 알고리즘들 
 	threshold( bitable , bitable, 128, 255, ADAPTIVE_THRESH_GAUSSIAN_C | CV_THRESH_BINARY | CV_THRESH_OTSU );       
 
-	imshow("CAM_Window", bitable); //얻어온 이미지를 윈도우에 표시
+	imshow("CAM_Window", bitable); //이진화까지 적용된 이미지를 윈도우에 표시
 //	imshow("Second_CAM", gray_image);
         char ch = waitKey(1);
 		if(ch == 27)
