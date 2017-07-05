09ccb74005a86188eafac0c63828421248d92b6e
Sanghoon Lee <gcccompil3r@gmail.com>
2017-07-05 22:51:25 +0900

    Kalman main
    gogogo

--- experiment/control_theory/quad_kal.c
+++ experiment/control_theory/quad_kal.c
@@ -27,10 +27,10 @@ void main(void) {
 
 	kalman_init(&roll_kalman_state, dt, 1.002, 0.001, 0.003);
 	kalman_init(&pitch_kalman_state, dt, 1.002, 0.001, 0.003);
-
+	
 	while(1) 
 	{
-
+		
 		dt += 0.0025;
 
 		// Get the gyro and accelerometer input.
@@ -44,12 +44,12 @@ void main(void) {
 		// Update the Kalman filter and get the output.
 		roll_kalman_output = kalman_update(&roll_kalman_state, roll_gyro_input, roll_accel_input);
 		printf(" roll_gyro(x) = %7.3f deg/sec    roll_accel(x)=%7.3f deg    roll_kalman_output(x)=%10.4f deg\n",
-				roll_gyro_input, roll_accel_input, roll_kalman_output);
+			roll_gyro_input, roll_accel_input, roll_kalman_output);
 
 		pitch_kalman_output = kalman_update(&pitch_kalman_state, pitch_gyro_input, pitch_accel_input);
 		printf(" pitch_gyro(x)=%7.3f deg/sec    pitch_accel(x)=%7.3f deg    pitch_kalman_output(x)=%10.4f deg\n",
-				pitch_gyro_input, pitch_accel_input, pitch_kalman_output);
+			pitch_gyro_input, pitch_accel_input, pitch_kalman_output);
 		pid(roll_kalman_output, pitch_kalman_output);
-
+	
 	}
 }
