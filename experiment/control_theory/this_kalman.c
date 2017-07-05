09ac284d0d99053f7535901289b30b41dee31b84
Sanghoon Lee <gcccompil3r@gmail.com>
2017-07-05 22:57:30 +0900

    Kalman Filter
    gogogo

--- experiment/control_theory/this_kalman.c
+++ experiment/control_theory/this_kalman.c
@@ -22,7 +22,7 @@ kalman roll_kalman_state;
 kalman pitch_kalman_state;
 
 void kalman_init(kalman *self, float dt, float Sz_00, float Sw_00, float Sw_11)
-	// Initialize the kalman state.
+// Initialize the kalman state.
 {
 	// Initialize outputs.
 	self->x_00 = 0.0;
@@ -53,71 +53,74 @@ void kalman_init(kalman *self, float dt, float Sz_00, float Sw_00, float Sw_11)
 
 float kalman_update(kalman *self,  float gyro_rate,  float accel_angle)
 {
-	// Inputs.
-	float u = gyro_rate;
-	float y = accel_angle;
+			// Inputs.
+			float u = gyro_rate;
+			float y = accel_angle;
 
-	// Temp.
-	float s_00;
-	float inn_00;
-	float K_00;
-	float K_10;
-	float AP_00;
-	float AP_01;
-	float AP_10;
-	float AP_11;
-	float APAT_00;
-	float APAT_01;
-	float APAT_10;
-	float APAT_11;
+				// Temp.
+			float s_00;
+			float inn_00;
+			float K_00;
+			float K_10;
+			float AP_00;
+			float AP_01;
+			float AP_10;
+			float AP_11;
+			float APAT_00;
+			float APAT_01;
+			float APAT_10;
+			float APAT_11;
 
 
-	/*  Update the state estimate by extrapolating current state estimate with input u.
-	    x = A * x + B * u
-	 */
-	self->x_00 = (self->A_01 * self->x_10) + (self->B_00 * u);
+			/*  Update the state estimate by extrapolating current state estimate with input u.
+			x = A * x + B * u
+			추정값 x_00 -> 칼만필터에서 출력값 deg
+			여기선 시간 업데이트 ->> 예측값
+			*/
+			self->x_00 = (self->A_01 * self->x_10) + (self->B_00 * u);
 
-	/*  Compute the innovation -- error between measured value and state.
-	    inn = y - c * x
-	 */
-	inn_00 = y - self->x_00;
+			/*  Compute the innovation -- error between measured value and state.
+			inn = y - c * x
+			예측값에서 예측한 x_00가 측정 업데이트로 넘어가면서
+			*/
+			inn_00 = y - self->x_00;
 
-	// Compute the covariance of the innovation.
-	// s = C * P * C' + Sz
-	s_00 = self->P_00 + self->Sz_00;
+			// Compute the covariance of the innovation.
+			// s = C * P * C' + Sz
+			s_00 = self->P_00 + self->Sz_00;
 
-	// Compute AP matrix for use below.
-	// AP = A * P
-	AP_00 = self->P_00 + self->A_01 * self->P_10;
-	AP_01 = self->P_01 + self->A_01 * self->P_11;
-	AP_10 = self->P_10;
-	AP_11 = self->P_11;
+			// Compute AP matrix for use below.
+			// AP = A * P
+			AP_00 = self->P_00 + self->A_01 * self->P_10;
+			AP_01 = self->P_01 + self->A_01 * self->P_11;
+			AP_10 = self->P_10;
+			AP_11 = self->P_11;
 
-	// Compute the kalman gain matrix.
-	// K = A * P * C' * inv(s)
-	K_00 = AP_00 / s_00;
-	K_10 = AP_10 / s_00;
+			// Compute the kalman gain matrix.
+			// K = A * P * C' * inv(s)
+			K_00 = AP_00 / s_00;
+			K_10 = AP_10 / s_00;
 
-	// Update the state estimate.
-	// x = x + K * inn
-	self->x_00 += K_00 * inn_00;
-	self->x_10 += K_10 * inn_00;
+			// Update the state estimate.
+			// x = x + K * inn
+			self->x_00 += K_00 * inn_00;
+			self->x_10 += K_10 * inn_00;
 
-	//	printf("k_00 = %f k_10 = %f\n ", K_00, K_10);
+			//	printf("k_00 = %f k_10 = %f\n ", K_00, K_10);
 
-	// Compute the new covariance of the estimation error.
-	// P = A * P * A' - K * C * P * A' + Sw
-	APAT_00 = AP_00 + (AP_01 * self->A_01);
-	APAT_01 = AP_01;
-	APAT_10 = AP_10 + (AP_11 * self->A_01);
-	APAT_11 = AP_11;
+			// Compute the new covariance of the estimation error.
+			// P = A * P * A' - K * C * P * A' + Sw
+			APAT_00 = AP_00 + (AP_01 * self->A_01);
+			APAT_01 = AP_01;
+			APAT_10 = AP_10 + (AP_11 * self->A_01);
+			APAT_11 = AP_11;
 
-	self->P_00 = APAT_00 + self->Sw_00;
-	self->P_01 = APAT_01;
-	self->P_10 = APAT_10;
-	self->P_11 = APAT_11 + self->Sw_11;
+			self->P_00 = APAT_00 + self->Sw_00;
+			self->P_01 = APAT_01;
+			self->P_10 = APAT_10;
+			self->P_11 = APAT_11 + self->Sw_11;
 
-	// Return the estimate.
-	return self->x_00;
-}
-
+			// Return the estimate.
+			return self->x_00;
+	}
+	
